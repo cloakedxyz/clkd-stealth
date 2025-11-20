3004fed00db84d0f7a1c3f50cf90903571971d15
Olivia Barnett <oliviabarnett@me.com>
2025-11-20 15:26:50 +0000

    fix: top level index

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+// Export all client functions
+export {
+  genKeysFromSignature,
+  genStealthPrivateKey,
+  genCloakedMessage,
+} from './client';
+
+// Export all shared functions
+export {
+  deriveChildViewingNode,
+  deriveDeterministicEphemeralKey,
+  genStealthAddress,
+  genStealthAddresses,
+} from './shared';
+
